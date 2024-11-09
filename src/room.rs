3075667289229d2ae4406ad58d7659720dbae6e9
ramanbhandari <ramanbhandarieoes@gmail.com>
2024-11-08 18:08:26 -0600

    one mod missed

--- src/room.rs
+++ src/room.rs
@@ -1,10 +1,9 @@
 use std::collections::HashMap;
-
+use crate::item::Item;
 pub struct Room {
     name: String,
     description: String,
     connections: HashMap<String, String>,
     items: Vec<Item>,
     is_locked: bool,
-    puzzle: Option<Puzzle>
 }
